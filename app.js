49bb96bd71e3c7816af78c9f1a510d1c44584604
Giacomo-Marconi <19894@studenti.marconiverona.edu.it>
2025-05-28 21:44:19 +0200

    fix: Rimuovi la restrizione CORS per le origini consentite

--- app.js
+++ app.js
@@ -31,11 +31,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, true)
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
